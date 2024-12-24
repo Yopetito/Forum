8daf8f00a4721826777fdadb25d0709c63b4cc88
yopetito <yofer.frg@gmail.com>
2024-12-24 12:00:53 +0100

    CSS de la navbar

--- view/layout.php
+++ view/layout.php
@@ -7,6 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
         <title>FORUM</title>
     </head>
@@ -19,7 +20,6 @@
                 <header>
                     <nav>
                         <div id="nav-left">
-                            <a href="/">Accueil</a>
                             <?php
                             if(App\Session::isAdmin()){
                                 ?>
@@ -31,10 +31,18 @@
                             // si l'utilisateur est connecté 
                             if(App\Session::getUser()){
                                 ?>
-                                <a href="index.php?ctrl=security&action=profile"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
-                                <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
-                                <a href="index.php">Page principale</a>
-                                <a href="index.php?ctrl=forum&action=index">Liste des catégories</a>
+                                <div class="nav-profile">
+                                    <a href="index.php?ctrl=security&action=profile"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
+                                </div>
+                                <div class="nav-logout">
+                                <a href="index.php?ctrl=security&action=logout"><span class="fa-solid fa-share-from-square"></span>Déconnexion</a>   
+                                </div>
+                                <div class="nav-homepage">
+                                <a href="index.php"><span class="fa-solid fa-house"></span>Page principale</a>
+                                </div>
+                                <div class="nav-category">
+                                <a href="index.php?ctrl=forum&action=index"><span class="fa-solid fa-layer-group"></span>Liste des catégories</a>
+                                </div>
                                 <?php
                             }
                             else{
