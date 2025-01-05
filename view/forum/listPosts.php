487e9c6f24557aacf3776a62415816860cc56a7e
yopetito <yofer.frg@gmail.com>
2025-01-05 13:04:57 +0100

    Message d'erreur si on insere un ID dans l'url directemente dans topics et posts

--- view/forum/listPosts.php
+++ view/forum/listPosts.php
@@ -1,28 +1,30 @@
 <?php
-    $topic = $result["data"]['topic']; 
-    $posts = $result["data"]['posts']; 
+$topic = $result["data"]['topic']; 
+$posts = $result["data"]['posts']; 
+if(!empty($topic) && !empty($posts)) { 
     $categoryId = $topic->getCategory()->getId();
-?>
-
-<h1><?= $topic->getTitle() ?> </h1>
-<a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?= $categoryId ?>" class="btn-return"><i class="fa-solid fa-rotate-left"> topics</i></a>
-<div class="list-box">
-    <?php 
-    
-    // compteur pour creation de la class dynamique pour du CSS
-    $i = 0;
-    foreach($posts as $post ){ 
-    $class = $i % 2 === 0 ? 'even' : 'odd'; ?>
-    <div class="list-item <?= $class ?>">
-        <!-- affichage de l'information des posts dans un tipic a l'aide des getters -->
-        <p>par <?= $post->getUser() ? $post->getUser() : "utilisateur supprimé"  ?> 
-        <br>le <?= $post->getCreationDate() ?> <br> <?= $post ?><br><br> </p>
+    ?>
+    <h1><?= $topic->getTitle() ?> </h1>
+    <a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?= $categoryId ?>" class="btn-return"><i class="fa-solid fa-rotate-left"> topics</i></a>
+    <div class="list-box">
+        <?php 
+        
+        // compteur pour creation de la class dynamique pour du CSS
+        $i = 0;
+        foreach($posts as $post ){ 
+        $class = $i % 2 === 0 ? 'even' : 'odd'; ?>
+        <div class="list-item <?= $class ?>">
+            <!-- affichage de l'information des posts dans un tipic a l'aide des getters -->
+            <p>par <?= $post->getUser() ? $post->getUser() : "utilisateur supprimé"  ?> 
+            <br>le <?= $post->getCreationDate() ?> <br> <?= $post ?><br><br> </p>
+        </div>
+        <!--  incrémentation de la var $i pour attribution de "odd" "even" dans le nom de la class-->
+        <?php $i++; ?>
+        <?php } ?>
     </div>
-    <!--  incrémentation de la var $i pour attribution de "odd" "even" dans le nom de la class-->
-    <?php $i++; ?>
-    <?php } ?>
-</div>
-<?php
+<?php } else {
+    echo "Topic non existant";
+} 
 
 //Si l'utilisateur est connecté : formulaire de création d'un nouveau topic avec un premier message
 if(App\Session::getUser()) {
--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -1,51 +1,54 @@
 <?php
-    use app\Session;
-    $category = $result["data"]['category']; 
-    $topics = $result["data"]['topics']; 
-?>
-<!-- ===========Topics dans la category : getName========= -->
-<h1><?= $category->getName() ?></h1>
-<a href="index.php?ctrl=forum&action=index" class="btn-return"><i class="fa-solid fa-rotate-left"> Categories</i></a>
-<div class="list-box">
-    <?php
-    
-    // compteur pour creation de la class dynamique pour du CSS
-    $i = 0;
-    foreach($topics as $topic){ 
-    $class = $i % 2 === 0 ? 'even' : 'odd'; ?>
-    <p>
-        <div class="list-item <?= $class ?>"> <!-- div avec la class dynamique -->
-            
-        <!-- affichage de l'information de chaque topic dans la categorie a l'aide des getters -->
-            <div class="topic-info">
-                <a href="index.php?ctrl=post&action=listPostInTopic&id=<?= $topic->getId() ?>"><?= $topic ?></a> 
-                <br> par <?= $topic->getUser() ? $topic->getUser() : "utilisateur supprimé" ?>  
-                <br> le <?= $topic->getCreationDate() ?>
-            </div>
-            
-            <!-- Vérouillage du topic >>>> si l'utilisateur est connecté && utilateur est propriaitre du topic || l'utilisateur a le role ADMIN  -->
-            <?php if(App\Session::getUser() && ($topic->getUser() && App\Session::getUser()->getId() == $topic->getUser()->getId()) || App\Session::isAdmin()) { ?>
-                <div class="lock-button">
-                    
-                    <a href="index.php?ctrl=topic&action=lockTopic&id= <?= $topic->getId() ?>">
-                    <!-- Condition topic vérouillé : Affichage cadénas fermé : ouvert -->
-                    <?= $topic->getLocked() ? '<i class="fa-solid fa-lock"></i>' : '<i class="fa-solid fa-lock-open"></i>' ?>
-                    </a>
-                </div>
-               
-                <!-- bouton supression -->
-                 <div class="delete-button">
-                    <a href="index.php?ctrl=security&action=deleteTopic&id= <?= $topic->getId() ?>"><i class="fa-solid fa-trash"></i></a>
-                </div>
-            <?php } ?>
-        </div> 
+use app\Session;
+$category = $result["data"]['category']; 
+$topics = $result["data"]['topics']; 
+// Securisation de l'url
+if(!empty($topics) && !empty($category)) { ?>
+    <!-- ===========Topics dans la category : getName========= -->
+    <h1><?= $category->getName() ?></h1>
+    <a href="index.php?ctrl=forum&action=index" class="btn-return"><i class="fa-solid fa-rotate-left"> Categories</i></a>
+    <div class="list-box">
+        <?php
         
-        <!--  incrémentation de la var $i pour attribution de "odd" "even" dans le nom de la class-->
-        <?php $i++; ?>
-    </p>
-    <?php  } ?>
-</div>
-
+        // compteur pour creation de la class dynamique pour du CSS
+        $i = 0;
+        foreach($topics as $topic){ 
+        $class = $i % 2 === 0 ? 'even' : 'odd'; ?>
+        <p>
+            <div class="list-item <?= $class ?>"> <!-- div avec la class dynamique -->
+                
+            <!-- affichage de l'information de chaque topic dans la categorie a l'aide des getters -->
+                <div class="topic-info">
+                    <a href="index.php?ctrl=post&action=listPostInTopic&id=<?= $topic->getId() ?>"><?= $topic ?></a> 
+                    <br> par <?= $topic->getUser() ? $topic->getUser() : "utilisateur supprimé" ?>  
+                    <br> le <?= $topic->getCreationDate() ?>
+                </div>
+                
+                <!-- Vérouillage du topic >>>> si l'utilisateur est connecté && utilateur est propriaitre du topic || l'utilisateur a le role ADMIN  -->
+                <?php if(App\Session::getUser() && ($topic->getUser() && App\Session::getUser()->getId() == $topic->getUser()->getId()) || App\Session::isAdmin()) { ?>
+                    <div class="lock-button">
+                        
+                        <a href="index.php?ctrl=topic&action=lockTopic&id= <?= $topic->getId() ?>">
+                        <!-- Condition topic vérouillé : Affichage cadénas fermé : ouvert -->
+                        <?= $topic->getLocked() ? '<i class="fa-solid fa-lock"></i>' : '<i class="fa-solid fa-lock-open"></i>' ?>
+                        </a>
+                    </div>
+                
+                    <!-- bouton supression -->
+                    <div class="delete-button">
+                        <a href="index.php?ctrl=security&action=deleteTopic&id= <?= $topic->getId() ?>"><i class="fa-solid fa-trash"></i></a>
+                    </div>
+                <?php } ?>
+            </div> 
+            
+            <!--  incrémentation de la var $i pour attribution de "odd" "even" dans le nom de la class-->
+            <?php $i++; ?>
+        </p>
+        <?php  } ?>
+    </div>
+<?php } else { 
+    echo "Pas de topics dans cette category"; 
+    } ?>
 <!-- Si l'utilisateur est connecté : formulaire de création d'un nouveau topic avec un premier message -->
 <?php if(App\Session::getUser()) { ?>
 <form class="form-style" action="index.php?ctrl=topic&action=addTopic&id=<?= $category->getId(); ?>" method="POST">
