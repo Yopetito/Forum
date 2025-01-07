f001b99c9a8b476490a1385976b5275424d8f62b
yopetito <yofer.frg@gmail.com>
2025-01-07 20:31:19 +0100

    Verification si une couleur est en localstorage + Choix de la couleur a enregistrer en local storage

--- public/js/script.js
+++ public/js/script.js
@@ -7,16 +7,27 @@ const colors = document.querySelectorAll('.color');
 //     colorChoice.style.display = "none"
 // });
 
+//verif de la couleur stocké.
+const savedColor = localStorage.getItem('selectedColor');
+if (savedColor) {
+    document.documentElement.style.setProperty('--principal-color', savedColor);
+    document.documentElement.style.setProperty('--border-color', savedColor);
+    document.documentElement.style.setProperty('--box-color', savedColor);
+}
+
+//montrer ou pas la palette de couleurs
 settingsIcon.addEventListener('click', function() {
     colorChoice.style.display = colorChoice.style.display === 'none' ? 'block' : 'none';
 });
  
+//changer la couleur des var en css selon la couleur choisie
 colors.forEach(color => {
     color.addEventListener('click', function() {
         const selectedColor = this.getAttribute('data-color');
         document.documentElement.style.setProperty('--principal-color', selectedColor);
         document.documentElement.style.setProperty('--border-color', selectedColor);
         document.documentElement.style.setProperty('--box-color', selectedColor);
+        localStorage.setItem('selectedColor', selectedColor);
         colorChoice.style.display = 'none';
     });
 });
