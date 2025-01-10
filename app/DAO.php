56fdd50fae223b2fc2fde676973e19ca20e12e4e
yopetito <yofer.frg@gmail.com>
2025-01-10 08:46:43 +0100

    css

--- app/DAO.php
+++ app/DAO.php
@@ -15,7 +15,7 @@ abstract class DAO{
     private static $host   = 'mysql:host=127.0.0.1;port=3306';
     private static $dbname = 'forum_yofer';
     private static $dbuser = 'root';
-    private static $dbpass = '';
+    private static $dbpass = 'Jifomeye1207';
     private static $bdd;
 
     /**
